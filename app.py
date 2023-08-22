d19c9ebeb22b8dbccbc5e1a46b1b03e2a69ce481
TeraTian <frogjay@aliyun.com>
2023-08-22 08:13:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ from bottle import get, run, request
 @get('/get')
 def getName():
     name = request.query.name
-    return 'Hello Allied' + name;
+    return 'Hello Allied v ' + name;
 
 run(host='0.0.0.0', port='8888')
